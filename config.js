f04dff0f621bec3a72d24d9e41c79eb802a83ff2
BoyVn2k <82346184+BoyVn2k@users.noreply.github.com>
2022-08-18 09:27:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề wbb",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Toản",
+    introTitle: 'Toản',
+    introDesc: `có
+    cái
+    nịt
+    nhá`,
+    btnIntro: '^^hí^^',
+    title: 'Toản 🥰',
+    desc: 'Toản nè ',
+    btnYes: 'Ghét lắm <33',
+    btnNo: 'Cóa nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình fa<3',
+    btnReply: 'Gửi cho bản thân <3',
+    reply: 'Yêu thì yêu mà không yêu thì cút <33333333',
+    mess: 'Mình biết mà 🥰. ell Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'off nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/toanpri.pham'
 }
